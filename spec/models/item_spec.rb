0865897066ff8a9ca38a813ee0e2ab0dd59c3c50
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-11-07 00:04:31 +0200

    adding more item specs

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe Item, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
   context "validations" do
-  	it { should allow_value('taken', 'free').for(:status) }
+  	it { should allow_value('taken', 'free')
+  							.for(:status) }
+  	it { should_not allow_value('jfowiejfo', 'kishkush', 'jfoeijfoe').for(:status) }
   end
 
   context "associations" do
