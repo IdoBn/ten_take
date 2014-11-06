96acbf7724d495f0d9a6f69db88ef25a35e0d4fb
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-11-07 00:02:17 +0200

    adding item specs

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Item, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  context "validations" do
+  	it { should allow_value('taken', 'free').for(:status) }
+  end
+
+  context "associations" do
+  	it { should belong_to(:user) }
+  end
 end
