98dc64da0a7bffe5ac6e88cfab79ec49cd0278b6
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-11-06 23:40:34 +0200

    fixing email fixture

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module TenTake
     config.generators do |g|
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
+
+    config.action_mailer.default_url_options = { host: "localhost:3000" }
   end
 end
