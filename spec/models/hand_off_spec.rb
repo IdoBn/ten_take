86586f65c674315afc13ba07c6ff5688e5cb27e4
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-11-07 18:02:27 +0200

    unit testing hand_off.next_step

--- spec/models/hand_off_spec.rb
+++ spec/models/hand_off_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe HandOff, :type => :model do
   context "asociations" do
   	it { should belong_to(:borrow) }
   end
+
+  it "should not allow more than 4 steps" do
+  	hand_off = FactoryGirl.create(:hand_off)
+  	expect(hand_off.status).to be(1)
+  	hand_off.next_step
+  	expect(hand_off.status).to be(2)
+  	hand_off.next_step
+  	expect(hand_off.status).to be(3)
+  	hand_off.next_step
+  	expect(hand_off.status).to be(4)
+  	hand_off.next_step
+  	expect(hand_off.status).to be(4)
+  end
 end
