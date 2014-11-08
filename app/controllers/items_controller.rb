00a0066ca72f4221acfb993bd13e79cca9cf34c5
Ido Ben-Natan <IdoBN@nimrod-bennatans-macbook-pro.local>
2014-11-08 11:30:32 +0200

    adding items controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,33 @@
+class ItemsController < ApplicationController
+  def index
+  	@items = Item.all
+  	render json: {items: @items}
+  end
+
+  def show
+  	@item = Item.find(params[:id])
+  	render json: {item: @item}
+  end
+
+  def update
+  	@item = Item.find(params[:id])
+  	if @item.update_attributes(params[:item])
+  		render json: {item: @item}
+  	else
+  		render json: {errors: @item.errors.full_messages}
+  	end
+  end
+
+  def create
+  	@item = Item.new(params[:item])
+  	if @item.save!
+  		render json: {item: @item}
+  	else
+  		render json: {errors: @item.errors.full_messages}
+  	end
+  end
+
+  def item_params
+  	params.require(:item).permit(:status)
+  end
+end
