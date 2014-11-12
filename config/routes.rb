c3117775c9f5276235a15052cc605720af4dec22
Ido Ben-Natan <IdoBN@nimrod-bennatans-macbook-pro.local>
2014-11-12 16:57:56 +0200

    added user's controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
   resources :hand_offs ,only: [:create,:update]
   resources :items
   resources :borrows
+  resources :users
   mount_devise_token_auth_for 'User', at: '/auth'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
