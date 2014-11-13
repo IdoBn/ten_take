1bc0aba117dd4572583d65e8071a08c50d13e360
Ido Ben-Natan <IdoBN@nimrod-bennatans-macbook-pro.local>
2014-11-14 00:44:08 +0200

    as_json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
   	@users = User.all
-  	render json: @users, each_serializer: UserSerializer
+  	render json: { users: @users.map { |u| UserSerializer.new(u) }.as_json(root: false) }
   end
 
   def show
   	@user = User.find(params[:id])
-  	render json: UserWithItemSerializer.new(@user).to_json
+  	render json: UserWithItemSerializer.new(@user).as_json
   end
 end
